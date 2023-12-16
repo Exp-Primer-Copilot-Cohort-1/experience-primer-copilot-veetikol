063b942a2c0bdc9a6cf7b7242359fef3b2f24da6
veetikol <11076348+veetikol@users.noreply.github.com>
2023-12-16 23:22:30 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,11 +1,64 @@
-const http = require('http');
+// Create web server with Express
+// To run: node comments.js
+// To test: curl http://localhost:3000/comments
+// To test: curl http://localhost:3000/comments/1
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, world!');
-});
+var express = require("express");
+var app = express();
+var fs = require("fs");
+var bodyParser = require('body-parser');
 
-server.listen(3000, 'localhost', () => {
-    console.log('Server running at http://localhost:3000/');
-});
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(express.static('public'));
+
+// GET /comments
+app.get('/comments', function (req, res) {
+   fs.readFile( __dirname + "/" + "comments.json", 'utf8', function (err, data) {
+       console.log( data );
+       res.end( data );
+   });
+})
+
+// GET /comments/id
+app.get('/comments/:id', function (req, res) {
+   // First read existing comments.
+   fs.readFile( __dirname + "/" + "comments.json", 'utf8', function (err, data) {
+       var comments = JSON.parse( data );
+       var comment = comments["comment" + req.params.id] 
+       console.log( comment );
+       res.end( JSON.stringify(comment));
+   });
+})
+
+// POST /comments
+// To test: curl -d "id=3&body=body3&author=author3&date=3/3/2017" -X POST http://localhost:3000/comments
+app.post('/comments', urlencodedParser, function (req, res) {
+
+   // First read existing comments.
+   fs.readFile( __dirname + "/" + "comments.json", 'utf8', function (err, data) {
+       data = JSON.parse( data );
+       var id = req.body.id;
+       var comment = req.body;
+       data["comment" + id] = comment;
+       console.log( data );
+       res.end( JSON.stringify(data));
+   });
+})
+
+// DELETE /comments/id
+// To test: curl -X DELETE http://localhost:3000/comments/3
+app.delete('/comments/:id', function (req, res) {
+
+   // First read existing comments.
+   fs.readFile( __dirname + "/" + "comments.json", 'utf8', function (err, data) {
+       data = JSON.parse( data );
+       var id = req.params.id;
+       delete data["comment" + id];
+       console.log( data );
+       res.end( JSON.stringify(data));
+   });
+})
+
+// PUT
